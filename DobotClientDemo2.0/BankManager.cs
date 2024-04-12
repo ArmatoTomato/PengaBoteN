e81412a38c98b292a3617c6cf5013b41f85b5950
OGShadowFalcon <anton.thornwall@telia.com>
2024-04-12 23:06:38 +0200

    Kommenterat och tagit bort user klass

--- DobotClientDemo2.0/BankManager.cs
+++ DobotClientDemo2.0/BankManager.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 
 namespace DobotClientDemo
 {
-    internal class BankManager
+    internal class BankManager//Klass som hanterar bank relaterade metoder och vissa användar funtkioner
     {
         DataBaseSQL db;
 
-        public void CreateAccount(string name, int balance, string password, string temp)
+        public void CreateAccount(string name, int balance, string password, string temp)//Skapar kontot
         {
             db = new DataBaseSQL("ATM.db");
             db.AddUser(name, balance, password, temp);
         }
         
-        public bool ChekExistingAmount(int id, int amount)
+        public bool ChekExistingAmount(int id, int amount)//Kollar hur mycket pengar som finns och om uttaget är möjlugt
         {
             db = new DataBaseSQL("ATM.db");
             int existingAmount = db.GetBalance(id);
@@ -29,7 +29,7 @@ namespace DobotClientDemo
                 return false;
             }
         }
-        public void WithdrawAmount(int id, int amount)
+        public void WithdrawAmount(int id, int amount)//Updaterar saldot
         {
             db = new DataBaseSQL("ATM.db");
             int existingAmount = db.GetBalance(id);
@@ -42,7 +42,7 @@ namespace DobotClientDemo
             int balance = db.GetBalance(id);
             return balance;
         }
-        public void DepositAmount(int id, int amount)
+        public void DepositAmount(int id, int amount)//Motsatt withdraw
         {
             db = new DataBaseSQL("ATM.db");
             int existingAmount = db.GetBalance(id);
